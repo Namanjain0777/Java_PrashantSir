77635fa608532ec565b81a5ba9c52b6a8a0d08a1
Naman Jain <Namanjainp01@gmail.com>
2024-10-14 18:55:01 +0530

    Create Problem3.java

---
+++ Problem3.java
@@ -0,0 +1,10 @@
+//Your can make another pattern like this
+public class Problem3 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * * ");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+    }
+}
