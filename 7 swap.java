2cf57056909e3365c5b4870a73261e4ae9112cc8
Naman Jain <Namanjainp01@gmail.com>
2024-10-19 15:38:07 +0530

    Create 7 swap.java

---
+++ 7 swap.java
@@ -0,0 +1,11 @@
+public class swap {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int temp = a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
