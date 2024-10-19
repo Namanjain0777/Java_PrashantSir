6c61b08bed428cdca59cb77e7ba6618486f06cf1
Naman Jain <Namanjainp01@gmail.com>
2024-10-19 15:46:49 +0530

    Create 19 grade.java

---
+++ 19 grade.java
@@ -0,0 +1,18 @@
+public class grade {
+        public static void main(String[] args) {
+        int marks = 85; 
+        
+        if (marks > 90) {
+            System.out.println('A');
+        } else if (marks > 75) {
+            System.out.println('B');
+        } else if (marks > 60) {
+            System.out.println('C');
+        } else if (marks > 30) {
+            System.out.println('D');
+        } else {
+            System.out.println('F');
+        }
+
+    }
+}
