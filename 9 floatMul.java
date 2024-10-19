9038d9f97009e0303591329ea3ac601b7b5c1d1f
Naman Jain <Namanjainp01@gmail.com>
2024-10-19 15:39:22 +0530

    Create 9 floatMul.java

---
+++ 9 floatMul.java
@@ -0,0 +1,8 @@
+public class floatMul {
+    public static void main(String[] args) {
+        float a = 3;
+        float b =4;
+        float result = a*b;
+        System.out.println(result);
+    }
+}
