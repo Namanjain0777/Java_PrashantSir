e01e03d220411a3b8e85469e454ba6bdfa3fc52a
Naman Jain <Namanjainp01@gmail.com>
2024-10-19 15:44:52 +0530

    Create 15 checkNumber.java

---
+++ 15 checkNumber.java
@@ -0,0 +1,14 @@
+public class checkNumber {
+    public static void main(String[] args) {
+        int n = 5;
+        if (n==0){
+            System.out.println("Number is zero");
+        }
+        else if (n<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Potive");
+        }
+    }         
+}
