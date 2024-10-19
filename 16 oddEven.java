1ebb6dc021135b1985b5011fe2269b446b03633f
Naman Jain <Namanjainp01@gmail.com>
2024-10-19 15:45:20 +0530

    Create 16 oddEven.java

---
+++ 16 oddEven.java
@@ -0,0 +1,11 @@
+public class oddEven {
+    public static void main(String[] args) {
+        int n = 5;
+        if (n%2==1){
+            System.out.println("Odd");
+        }
+        else {
+            System.out.println("Even");
+        }
+    }
+}
