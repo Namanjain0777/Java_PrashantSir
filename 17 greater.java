5809b0fa344b32d483f2fe98b52b9cd7949061b5
Naman Jain <Namanjainp01@gmail.com>
2024-10-19 15:45:49 +0530

    Create 17 greater.java

---
+++ 17 greater.java
@@ -0,0 +1,16 @@
+public class greater {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 9;
+        int c = 5;
+        if (a>b && a>c){
+            System.out.println("a is greater");
+        }
+        else if (b>c){
+            System.out.println("B is Greater");
+        }
+        else {
+            System.out.println("C is Greater");
+        }
+    }
+}
