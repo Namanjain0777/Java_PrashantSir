13881b55cdc602d7e6356fff3e92d69202787f60
Naman Jain <Namanjainp01@gmail.com>
2024-10-19 15:47:10 +0530

    Create 20 age.java

---
+++ 20 age.java
@@ -0,0 +1,21 @@
+public class age {
+        public static void main(String[] args) {
+        int age = 25; // example age
+        if (age < 13) {
+            System.out.println("Child"  );
+
+        } else if (age < 20) {
+            System.out.println("Teen"  );
+
+        } else if (age < 60) {
+            System.out.println("Adult"  );
+
+        } else {
+            System.out.println("Senior"  );
+
+        }
+        
+}
+}
+
+    
