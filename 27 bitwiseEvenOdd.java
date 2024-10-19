f8fc21f7a22c2fba87a2ddf52b0cddfc3990d19d
Naman Jain <Namanjainp01@gmail.com>
2024-10-19 15:52:27 +0530

    Create 27 bitwiseEvenOdd.java

---
+++ 27 bitwiseEvenOdd.java
@@ -0,0 +1,11 @@
+public class bitwiseEvenOdd {
+    public static void main(String[] args) {
+        int num = 4;
+        if ( (num & 1) ==1){
+            System.out.println("number is odd");
+        }else{
+            System.out.println("EVEN");
+        }
+        
+}
+}
