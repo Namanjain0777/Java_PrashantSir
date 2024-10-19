a7a1935cc80df137a65625ee3dfcee7e0f0a3e4f
Naman Jain <Namanjainp01@gmail.com>
2024-10-19 15:47:41 +0530

    Create bitWiseAnd.java

---
+++ bitWiseAnd.java
@@ -0,0 +1,8 @@
+public class bitWiseAnd {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int result = a & b;
+        System.out.println(result);
+    }
+}
